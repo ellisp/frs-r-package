e112caf488782f3d57bedca1a5cdc329321ac511
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-07-06 13:53:47 +1200

    add general frs documentation

---
+++ pkg/R/frs.R
@@ -0,0 +1,22 @@
+#' Free Range Statistics
+#' 
+#' Miscellaneous odds and ends associated with the Free Range Statistics blog
+#' 
+#' These are items that have been used in writing posts for \link{http://freerangestats.info} or at least
+#' thinking about writing posts for it.
+#'  
+#' So far, the bits and pieces include:
+#' \itemize{
+#'     \item{dual axis plots}
+#'     \item{so-called "modulus" transformation for ggplot2 scales, which is like a Box Cox of the 
+#'     absolute value then with the sign returned.  Excellent for visualising economic variables that 
+#'     can be zero or negative}
+#'     \item{French death rates data in 2015}
+#'     \item{Colour palette and ggplot2 theme for International Finance Corporation branding 
+#'     (disclaimer - not associated at all with the IFC, but I did use it successfully in a 
+#'     report for them once)}
+#'     \item{turn levels of a factor into letters, useful for quick and dirty confidentialisation}
+#'     \item{some ODBC database odds and ends to help with R interacting with SQL Server}
+#' }
+#' 
+"_PACKAGE"
