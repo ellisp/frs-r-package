be7bb58c556060618299f02fca73e0be16e26223
Peter Ellis <peter.ellis2013nz@gmail.com>
2019-03-26 19:58:45 +1100

    Elo ratings allow home town advantage

--- pkg/R/elo_rating.R
+++ pkg/R/elo_rating.R
@@ -5,6 +5,8 @@
 #' @param a Elo rating of player a
 #' @param b Elo rating of player b
 #' @param ml length of match (if a backgammon game, the minimum winning score)
+#' @param a_adv any arbitrary advantage that player a gets on top of the usual comparison of ratings,
+#' for example from home town advantage
 #' @export
 #' @examples 
 #' # 50% chance of winning if both players have equal ratings:
@@ -12,10 +14,13 @@
 #' 
 #' # 87% chance of winning a match to 17 if you're a backgammon expert v average player:
 #' elo_prob(1900, 1500, 17)
-elo_prob <- function(a, b, ml){
+#' 
+#' # if you have a home town advantage of 10% but are otherwise equal:
+#' elo_prob(1700, 1700, 31, 0.1)
+elo_prob <- function(a, b, ml, a_adv = 0){
   # 
   # against player b of length ml, with a and b representing their FIBS Elo ratings
-  tmp <- 1 - (1 / (10 ^ ((a - b) * sqrt(ml) / 2000) + 1))
+  tmp <- pmin(1 - (1 / (10 ^ ((a - b) * sqrt(ml) / 2000) + 1)) + a_adv, 1)
   return(tmp)
 }   
 
@@ -30,12 +35,17 @@ elo_prob <- function(a, b, ml){
 #' @param ml is match length
 #' @param axp total match lengths (experience) of player a until this match
 #' @param bxp total match lengths (experience) of player b until this match
+#' @param a_adv any arbitrary advantage that player a gets on top of the usual comparison of ratings,
+#' for example from home town advantage
 #' @details See http://www.fibs.com/ratings.html for formulae
 #' @examples 
 #' # baptism by fire; a's rating should be 1540.95 after beating a master player:
 #' round(elo_rating(a = 1500, b = 1925, ml = 7, axp = 0, bxp = 10000, winner = "a")$a, 2)
+#' 
+#' # short game where b has a home town advantage of 0.1
+#' elo_rating(1500, 1500, a_adv = -0.1)
 #' @author Peter Ellis
-elo_rating <- function(a, b, winner = c("a", "b"), ml = 1, axp = 500, bxp = 500){
+elo_rating <- function(a, b, winner = c("a", "b"), ml = 1, axp = 500, bxp = 500, a_adv = 0){
   winner <- match.arg(winner)
   
   
@@ -43,8 +53,8 @@ elo_rating <- function(a, b, winner = c("a", "b"), ml = 1, axp = 500, bxp = 500)
   multa <- ifelse(axp < 400, 5 - ((axp + ml) / 100), 1)
   multb <- ifelse(bxp < 400, 5 - ((axp + ml) / 100), 1)
   
-  # probability of A winning, using fibs_p function defined earlier:
-  winproba <- elo_prob(a = a, b = b, ml = ml)
+  # probability of A winning, using elo_prob:
+  winproba <- elo_prob(a = a, b = b, ml = ml, a_adv = a_adv)
   
   # match value (points to be distributed between the two players):
   matchvalue <- 4 * sqrt(ml)
